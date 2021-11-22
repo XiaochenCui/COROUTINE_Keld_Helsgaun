27e1691281b0d38de4208023afa1e8b94454e557
XiaochenCui <jcnlcxc.new@gmail.com>
2021-11-22 13:53:09 +0800

    stash changes

--- SRC/simset/simset.h
+++ SRC/simset/simset.h
@@ -1,20 +1,24 @@
 #ifndef SIMSET_H
 #define SIMSET_H
 
-class Linkage {
-friend class Link;
-friend class Head;
+class Linkage
+{
+    friend class Link;
+    friend class Head;
+
 public:
     Linkage();
     Link *Pred() const;
     Link *Suc() const;
     Linkage *Prev() const;
+
 private:
     Linkage *PRED, *SUC;
     virtual Link *LINK() = 0;
 };
 
-class Head : public Linkage {
+class Head : public Linkage
+{
 public:
     Head();
     Link *First() const;
@@ -22,16 +26,19 @@ public:
     int Empty(void) const;
     int Cardinal(void) const;
     void Clear(void);
+
 private:
     Link *LINK();
-}; 
+};
 
-class Link : public Linkage {
+class Link : public Linkage
+{
 public:
     void Out(void);
     void Into(Head *H);
     void Precede(Linkage *L);
     void Follow(Linkage *L);
+
 private:
     Link *LINK();
 };
